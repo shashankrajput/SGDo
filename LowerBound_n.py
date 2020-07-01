5fd18c002c8a708f98951fd5d666c667789ece54
shashankrajput <rajput.shashank11@gmail.com>
2020-07-01 15:48:56 -0500

    Update LowerBound_n.py

--- LowerBound_n.py
+++ LowerBound_n.py
@@ -2,35 +2,28 @@ import numpy as np
 import random
 import multiprocessing
 
-# lamda = 1.0
 G = 1.0
 sF=4.0
 
-def single_n(n):
+def paralleOptimization_n(n):
   print(n)
   stepFactor=sF
-  iters=500
-  # eta = 100.0/(n*lamda*iters)
-  eta = stepFactor * np.log(n*iters) / (n*iters)
+  K=500
+  eta = stepFactor * np.log(n*K) / (n*K)
   e3 = 0
   for rep in range(reps):
-    e3 += randomReshuffleNonLipHess(eta, n, iters)
+    e3 += randomReshuffleNonLipHess(eta, n, K)
 
   eps3_i = 1.0*e3/reps
   epsr1_i = 1.0/n
-  epsr2_i = 1.0*(np.log(iters*n)**2)/n
+  epsr2_i = 1.0*(np.log(K*n)**2)/n
   return [eps3_i, epsr1_i, epsr2_i]
 
-def randomReshuffleNonLipHess(eta, n, iters):
-  # x = np.zeros(iters)
-  # x[0] = 0
+def randomReshuffleNonLipHess(eta, n, K):
   x=0
-  for i in range(1, iters+1):
-    # eta=stepFactor/(i*n)
+  for i in range(1, K+1):
     r = np.random.permutation(np.concatenate((-np.ones(n/2), np.ones(n/2))))
-    # x[i] = x[i-1]
     for j in range(0, n):
-      # x[i] = (1 - eta*lamda*4)*x[i] - eta*lamda*4*r[j]
       if (x>0):
         x = (1 - eta*4.0)*x - eta*r[j]
       else:
@@ -38,14 +31,12 @@ def randomReshuffleNonLipHess(eta, n, iters):
 
   return x**2
 
-reps = 100 #1000
-# stepFactor=sF
-iters=500
+reps = 1000
 n_beg=30
 n_end=200
 x_list=[]
 n_range = range(n_beg,n_end,2)
 pool = multiprocessing.Pool(16)
-results = pool.map(single_n, n_range)
-f = open('plotdata/experiment4_parallel_'+str(sF), 'w')
+results = pool.map(paralleOptimization_n, n_range)
+f = open('plotdata/experiment4_parallel_'+str(sF), 'w') # Replace with desired output file name
 f.write(",".join([str(n) for n in n_range]) + "\n" + "\n".join([",".join([str(r) for r in res]) for res in results]))
